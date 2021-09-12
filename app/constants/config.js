d89a19b20b016c20253b8837ea1319fd736bddc6
thuantruong <thuan20132000@gmail.com>
2021-09-12 11:09:37 +0700

    update android config

--- app/constants/config.js
+++ app/constants/config.js
@@ -39,14 +39,13 @@ export default {
     // "local_absolute":"http://192.168.5.42:8000",
 
     // Advertisiment
-    // android
-    
-    // adbmod_android_app_id: "ca-app-pub-7783640686150605~6780496114",
-    // adbmod_android_banner: 'ca-app-pub-7783640686150605/2939455462',
-    // adbmod_android_fullpage: 'ca-app-pub-7783640686150605/2202116538',
+    // android 
+    adbmod_android_app_id: "ca-app-pub-7783640686150605~6780496114",
+    adbmod_android_banner: 'ca-app-pub-7783640686150605/2939455462',
+    adbmod_android_fullpage: 'ca-app-pub-7783640686150605/2202116538',
 
     // ios
-    adbmod_android_app_id: "ca-app-pub-7783640686150605~7080533923",
-    adbmod_android_banner: 'ca-app-pub-7783640686150605/1636635552',
-    adbmod_android_fullpage: 'ca-app-pub-7783640686150605/1921683340',
+    // adbmod_android_app_id: "ca-app-pub-7783640686150605~7080533923",
+    // adbmod_android_banner: 'ca-app-pub-7783640686150605/1636635552',
+    // adbmod_android_fullpage: 'ca-app-pub-7783640686150605/1921683340',
 }
